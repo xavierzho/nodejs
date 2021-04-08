7be9973fc5186282e3e9adcb2cec8dffef4f036a
jonescy <jonescyna@gmail>
2021-04-09 04:02:25 +0800

    Preliminary exploration of typescript

---
+++ study/typescript/statictpye.ts
@@ -0,0 +1,9 @@
+let num: number = 10
+num = 20
+
+let Person: { uname: string, age: number } = { uname: "jones", age: 18 }
+
+let stringArr: string[] = ["jones", "asd"]
+
+let numArr: number[] = [1, 2, 3]
+let orArr: (number | string)[] = ['123', 213]
---
+++ study/typescript/tuple.ts
@@ -0,0 +1,7 @@
+let turple: [string, number, string] = ['jones', 12, "jasd"]
+//应用：CSV格式
+let girl: [string, string, number][] = [
+    ['jones',"teacher",18],
+    ['jones',"teacher",18],
+    ['jones',"teacher",18]  
+]
